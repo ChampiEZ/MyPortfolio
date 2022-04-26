f61b92b4d50677903ff10152a5110e32b1bb1767
Adam <adam.ammar@edu.devinci.fr>
2022-04-26 15:09:40 +0200

    :tada: My first Commit !

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="https://www.1formatik.com/images/favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
+</head>
+
+<body>
+
+
+
+    <section class="container">
+
+
+        <!-- Home -->
+        <section class="v-slider-bloc" id="un" class="ouesh">
+
+            <div class="home">
+                <img src="img/bg.jpg" id="bg">
+                <img src="img/moon.png" id="moon">
+                <img src="img/road.png" id="road">
+                <h2 id="textM">"Concentrez-vous sur ce qui <br> vous rend différent."</h2>
+            </div>
+
+        </section>
+
+
+        <!-- About Me -->
+        <section class="v-slider-bloc" id="deux">
+
+            <div class="am">
+                <div class="tit">
+                    <h2><span class="swag">About Me </span></h2>
+                </div>
+                <div class="blocredi">
+                    <a href="#trois">
+                        <p>Télécharger mon CV ici!</p>
+                        <img src="img/arrow.png" alt="arrow">
+                    </a>
+                </div>
+                <div class="tBx">
+                    <h1>lorem</h1>
+                </div>
+            </div>
+
+        </section>
+
+
+
+        <!-- Skills -->
+        <section class="v-slider-bloc" id="trois">
+
+            <section class="skills-section my-skills" id="skills">
+                <h1 class="text-skills" data-aos="slide-up">Skills</h1>
+                <img src="https://raw.githubusercontent.com/Sacsam005/my_portfolio_website/0fc1dccb57e4a40e1751d695fbd5b8048fae6471/img/skills.svg"
+                    alt="Skills" data-aos="zoom-out">
+            </section>
+
+            <section class="skills" id="skills">
+                <div class="max-width">
+                    <div class="skills-content">
+                        <div class="column left">
+                            <div class="text">My creative skills & experience</div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto labore corrupti omnis
+                                totam,
+                                libero debitis fugit nulla accusantium repellat amet excepturi, facere, saepe
+                                praesentium.
+                                Aliquam provident voluptate ratione at distinctio.
+                                My creative skills & experience
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto labore corrupti
+                                omnis totam,
+                                libero debitis fugit nulla accusantium repellat amet excepturi, facere, saepe
+                                praesentium.
+                                Aliquam provident voluptate ratione at distinctio.</p>
+                            <a href="#">Read more</a>
+                        </div>
+                        <div class="column right">
+                            <div class="bars">
+                                <div class="info">
+                                    <span class="infoS">HTML</span>
+                                    <span class="infoS">90%</span>
+                                </div>
+                                <div class="line html"></div>
+
+                                <div class="info">
+                                    <span class="infoS">CSS</span>
+                                    <span class="infoS">70%</span>
+                                </div>
+                                <div class="line css"></div>
+                                <div class="info">
+                                    <span class="infoS">JavaScript</span>
+                                    <span class="infoS">60%</span>
+                                </div>
+
+                                <div class="line js"></div>
+                                <div class="info">
+                                    <span class="infoS">PHP</span>
+                                    <span class="infoS">20%</span>
+                                </div>
+                                <div class="line php"></div>
+                                <div class="info">
+                                    <span class="infoS">Adobe Suite</span>
+                                    <span class="infoS">70%</span>
+                                </div>
+                                <div class="line mysql"></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
+
+
+
+
+
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Project -->
+        <section class="v-slider-bloc" id="quatre">
+            <h1>Bloc 4</h1>
+        </section>
+
+
+
+
+
+
+
+        <!-- Contact -->
+        <section class="v-slider-bloc" id="cinq">
+            <section class="contact">
+                <div class="content">
+                    <h2>Write me a <span class="clr_title2">M</span>essage</h2>
+                    <span class="plitle">
+                        <p>Cette section est pour les personnes qui veulent me contacter dans l'avenir, me signaler un
+                            bug sur le site ou tout simplement m'écrire un petit message <span
+                                class="clr_title">:)</span></p>
+                    </span>
+                </div>
+                <div class="containerS">
+                    <div class="contactInfo">
+                        <div class="box box2">
+                            <div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                            <div class="text">
+                                <h3>Adresse</h3>
+                                <p>4671 Sugar Camp Road,<br>Owatonna,Minnesota,<br>55060</p>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                            <div class="text">
+                                <h3>Téléphone</h3>
+                                <p>07 67 77 93 50</p>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                            <div class="text">
+                                <h3>Email</h3>
+                                <p>adam.ammar@edu.devinci.fr</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contactForm">
+                        <form>
+                            <h2>Send Message</h2>
+                            <div class="inputBox">
+                                <input type="text" name="" required="required">
+                                <span>Full Name</span>
+                            </div>
+                            <div class="inputBox">
+                                <input type="text" name="" required="required">
+                                <span>Email</span>
+                            </div>
+                            <div class="inputBox">
+                                <textarea required="required"></textarea>
+                                <span>Type your Message...</span>
+                            </div>
+                            <div class="inputBox">
+                                <input type="submit" name="" value="Send">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+
+
+    </section>
+
+    <script src="script.js"></script>
+
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,43 @@
+// Anim Header
+
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0);
+});
+
+// Anim Typer
+
+const txtAnim = document.querySelector('h1');
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 20
+})
+.changeDelay(40)
+.typeString('Lorem <span style="color : #2d6a4f"> Vue</span> ipsum dolor sit amet consectetur adipisicing elit. Ea iste illum cumque? Tenetur perferendis quod voluptatibus molestias non cum numquam voluptatum sapiente? Quo ipsam vero odit, laudantium totam maxime provident')
+.pauseFor(300)
+.deleteChars(14)
+.typeString('Lorem ipsum dolor sit amet <span style="color : #ea39ff"> HTML</span> consectetur adipisicing elit. Ea iste illum cumque? Tenetur perferendis quod voluptatibus molestias non cum numquam voluptatum sapiente? Quo ipsam vero odit, laudantium totam maxime provident')
+.pauseFor(300)
+.deleteChars(5)
+.typeString('Lorem ipsum dolor <span style="color : midnightblue"> PHP</span> sit amet consectetur <span style="color : #27ae60"> SASS</span>')
+.pauseFor(300)
+.deleteChars(10)
+.start()
+
+
+// Anim Home
+
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("moutain");
+let road = document.getElementById("road");
+let text = document.getElementById("textM");
+
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    textM.style.top = value * 1 + 'px';
+})
